98afd4e1a7fe29e0940547996d1aad5e75512910
MCorbin39 <mcorbin39@ivytech.edu>
2026-03-23 20:49:25 -0400

    Add files via upload

--- Assignment1.java
+++ Assignment1.java
@@ -1,21 +1,25 @@
 public class Exercise06_09 {
 
+    // Convert feet to meters
     public static double footToMeter(double foot) {
         return 0.305 * foot;
     }
 
+    // Convert meters to feet
     public static double meterToFoot(double meter) {
         return 3.279 * meter;
     }
 
     public static void main(String[] args) {
 
+        // Table header
         System.out.printf("%-10s%-15s%-15s%-10s%n", 
                           "Feet", "Meters", "Meters", "Feet");
         System.out.println("------------------------------------------------------");
 
         double meterValue = 20.0;
 
+        // Row loop
         for (int i = 1; i <= 10; i++) {
             double meters = footToMeter(i);
             double feet = meterToFoot(meterValue);
