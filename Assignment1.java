c92fc226106421f8f4452daee2322a30fe66f117
MCorbin39 <mcorbin39@ivytech.edu>
2026-03-23 20:46:34 -0400

    Add files via upload

---
+++ Assignment1.java
@@ -0,0 +1,29 @@
+public class Exercise06_09 {
+
+    public static double footToMeter(double foot) {
+        return 0.305 * foot;
+    }
+
+    public static double meterToFoot(double meter) {
+        return 3.279 * meter;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.printf("%-10s%-15s%-15s%-10s%n", 
+                          "Feet", "Meters", "Meters", "Feet");
+        System.out.println("------------------------------------------------------");
+
+        double meterValue = 20.0;
+
+        for (int i = 1; i <= 10; i++) {
+            double meters = footToMeter(i);
+            double feet = meterToFoot(meterValue);
+
+            System.out.printf("%-10.1f%-15.3f%-15.1f%-10.3f%n",
+                    (double)i, meters, meterValue, feet);
+
+            meterValue += 5;
+        }
+    }
+}
